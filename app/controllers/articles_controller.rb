c02240df74c4a7acd8a5e4e039e3419530fe5107
Henrique Santana <henrique.santana@outlook.com>
2017-05-23 18:38:50 +0000

    Implement edit article feature

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,11 +2,11 @@ class ArticlesController < ApplicationController
   def index
     @articles = Article.all
   end
-  
+
   def new
     @article = Article.new
   end
-  
+
   def create
     @article = Article.new(article_params)
     if @article.save
@@ -17,13 +17,28 @@ class ArticlesController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @article = Article.find(params[:id])
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:success] = "Article has been updated"
+      redirect_to @article
+    else
+      flash.now[:danger] = "Article has not been updated"
+      render :edit
+    end
+  end
+
   protected
-  
+
   def resource_not_found
     message = "The article you are looking for could not be found"
     flash[:alert] = message
