e27cb49e04f6ae62b916f38f013c753d18b17eee
Henrique Santana <henrique.santana@outlook.com>
2017-05-25 20:58:40 +0000

    Fix broken show article specs

--- spec/features/editing_article_spec.rb
+++ spec/features/editing_article_spec.rb
@@ -2,9 +2,9 @@ require "rails_helper"
 
 RSpec.feature "Editing an article" do
   before do
-    @john =  User.create!(email:"john@example.com", password: "password")
-    login_as(@john)
-    @article = Article.create(title: "Title One", body: "Body of article one", user: @john)
+    john =  User.create!(email:"john@example.com", password: "password")
+    login_as(john)
+    @article = Article.create(title: "Title One", body: "Body of article one", user: john)
   end
 
   scenario "A user updates an article" do
--- spec/features/show_article_spec.rb
+++ spec/features/show_article_spec.rb
@@ -2,14 +2,16 @@ require "rails_helper"
 
 RSpec.feature "Showing an article" do
   before do
-    @article = Article.create(title: "Fisrt article", body: "Lorem ipsum dolor dit amet, consectetur.")
+    john =  User.create!(email:"john@example.com", password: "password")
+    login_as(john)
+    @article = Article.create(title: "Title One", body: "Body of article one", user: john)
   end
-  
+
   scenario "A user shows an article" do
     visit "/"
-    
+
     click_link @article.title
-    
+
     expect(page).to have_content (@article.title)
     expect(page).to have_content (@article.body)
     expect(current_path).to eq(article_path(@article))
