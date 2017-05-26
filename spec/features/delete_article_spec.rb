277a74981b496f1896e2102ed1e0f26ccd24cf46
Henrique Santana <henrique.santana@outlook.com>
2017-05-26 18:18:27 +0000

    Show new article button to signed in users

--- spec/features/delete_article_spec.rb
+++ spec/features/delete_article_spec.rb
@@ -3,6 +3,7 @@ require "rails_helper"
 RSpec.feature "Deleting an Article" do
   before do
     john =  User.create!(email:"john@example.com", password: "password")
+    login_as(john)
     @article = Article.create(title: "Title One", body: "Body of article one", user: john)
   end
 
